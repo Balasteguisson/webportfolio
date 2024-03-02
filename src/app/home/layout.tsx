94a7adfe65112becc082f55e3f0f0095e5e27c37
Balasteguisson <gbg.balas@gmail.com>
2024-03-02 14:37:31 +0100

    Añadido logo y hover para verlo en el nombre

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,11 +1,14 @@
 import '@/styles/layout.css'
 import PortfolioNavigation from '@/components/portfolioNavigation'
-
+import Image from 'next/image'
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="parentLayout">
       <div className="headerDiv">
-        <div className="logo">Guillermo.</div>
+        <div className="logo">
+          <p>Guillermo.</p>
+          <Image src='/nobcLogo.png' alt='Logo personal de Guillermo' width={180} height={60}/>
+        </div>
         <PortfolioNavigation></PortfolioNavigation>
       </div>
       <div>{children}</div>
